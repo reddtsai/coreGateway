c30028482f14d099109d42e64deb9c39fcdcc14b
ReddTsai <reddtsai@gmail.com>
2019-05-24 12:07:30 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # coreGateway
+
+## Start
+
+```bash
+dotnet new web
+dotnet add package Ocelot
+```
+
+```c#
+
+```
---
+++ Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace gateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseOcelot().Wait();
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+
+            // app.Run(async (context) =>
+            // {
+            //     await context.Response.WriteAsync("Hello World!");
+            // });
+        }
+    }
+}
